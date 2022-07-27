1aad14ce4d37499ec63fbe211f7673b86c595cb3
林兴 <linxing0812@vip.qq.com>
2022-07-27 17:13:04 +0800

    设置dtls

--- server/juansfu/udp/rtc_dtls.cpp
+++ server/juansfu/udp/rtc_dtls.cpp
@@ -1,7 +1,80 @@
 ﻿#include <juansfu/udp/rtc_dtls.h>
 #include <juansfu/utils/global.h>
-
+#include <juansfu/api/crypto/crypto_options.h>
 #include <iostream>
+
+DtlsStreamInterface::DtlsStreamInterface(uvcore::Udp* udp, const uvcore::IpAddress& remote_addr)
+	:_que(2048, 2),
+	_udp(udp),
+	_remote_addr(remote_addr)
+{}
+
+rtc::StreamState DtlsStreamInterface::GetState() const
+{
+	return _state;
+}
+
+rtc::StreamResult DtlsStreamInterface::Read(void* buffer,
+	size_t buffer_len,
+	size_t* read,
+	int* error)
+{
+	if (_state == rtc::SS_CLOSED) {
+		return rtc::SR_EOS;
+	}
+
+	if (_state == rtc::SS_OPENING) {
+		return rtc::SR_BLOCK;
+	}
+
+	if (!_que.ReadFront(buffer, buffer_len, read)) {
+		return rtc::SR_BLOCK;
+	}
+
+	return rtc::SR_SUCCESS;
+}
+
+rtc::StreamResult DtlsStreamInterface::Write(const void* data,
+	size_t data_len,
+	size_t* written,
+	int* error)
+{
+	_udp->send2((const char*)data, data_len, _remote_addr);
+	if (written) {
+		*written = data_len;
+	}
+
+	return rtc::SR_SUCCESS;
+}
+
+void DtlsStreamInterface::Close()
+{
+	_state = rtc::SS_CLOSED;
+	_que.Clear();
+}
+
+bool DtlsStreamInterface::on_packet_receive(const uint8_t* data, size_t len)
+{
+	size_t written_bytes;
+	bool flag = _que.WriteBack(data, len, &written_bytes);
+
+	SignalEvent(this, rtc::SE_READ, 0);
+	return flag;
+}
+
+/////////////////////////////////DTLS..//////////////////////
+
+
+RtcDtls::RtcDtls(uvcore::Udp* udp, const uvcore::IpAddress& remote_addr)
+{
+	auto downward = std::make_unique<DtlsStreamInterface>(udp, remote_addr);
+	DtlsStreamInterface* downward_ptr = downward.get();
+	_downward = downward_ptr;
+
+	_dtls_adapter = rtc::SSLStreamAdapter::Create(std::move(downward));
+	webrtc::CryptoOptions crypto_options;
+	_srtp_ciphers = crypto_options.GetSupportedDtlsSrtpCryptoSuites();
+}
 
 bool RtcDtls::is_dtls(const uint8_t* data, size_t len)
 {
@@ -28,6 +101,40 @@ void RtcDtls::set_remote_dtls(const std::string& alg, const std::string& fp)
 
 	_remote_alg = alg;
 	_remote_fp = fp;
+}
+
+bool RtcDtls::setup_dtls()
+{
+	if (_dtls_adapter)
+	{
+		return false;
+	}
+	_dtls_adapter->SetIdentity(Global::GetInstance()->get_dtls_certificate()->identity()->Clone());
+	_dtls_adapter->SetMode(rtc::SSL_MODE_DTLS);
+	_dtls_adapter->SetMaxProtocolVersion(rtc::SSL_PROTOCOL_DTLS_12);
+	_dtls_adapter->SetServerRole(rtc::SSL_SERVER);
+
+	if (_remote_fp.size() && !_dtls_adapter->SetPeerCertificateDigest(
+		_remote_alg,
+		(const unsigned char*)_remote_fp.c_str(),
+		_remote_fp.size()))
+	{
+		return false;
+	}
+
+	if (!_srtp_ciphers.empty()) 
+	{
+		if (!_dtls_adapter->SetDtlsSrtpCryptoSuites(_srtp_ciphers)) 
+		{
+			std::cerr << ": Failed to set DTLS-SRTP crypto suites" << std::endl;
+			return false;
+		}
+	}
+	else 
+	{
+		std::cerr << ": Not using DTLS-SRTP" << std::endl;
+		return false;
+	}
 }
 
 void RtcDtls::handle_dtls(const uint8_t* data, size_t len)
--- server/juansfu/udp/rtc_dtls.h
+++ server/juansfu/udp/rtc_dtls.h
@@ -3,6 +3,36 @@
 
 #include <stdint.h>
 #include <string>
+#include <juansfu/rtc_base/ssl_stream_adapter.h>
+#include <juansfu/rtc_base/buffer_queue.h>
+#include <vector>
+#include <uvnet/core/udp_server.h>
+#include <uvnet/core/ip_address.h>
+
+class DtlsStreamInterface : public rtc::StreamInterface
+{
+public:
+    DtlsStreamInterface(uvcore::Udp*, const uvcore::IpAddress& remote_addr);
+    rtc::StreamState GetState() const override;
+    rtc::StreamResult Read(void* buffer,
+        size_t buffer_len,
+        size_t* read,
+        int* error) override;
+    rtc::StreamResult Write(const void* data,
+        size_t data_len,
+        size_t* written,
+        int* error) override;
+    void Close() override;
+
+    bool on_packet_receive(const uint8_t* data, size_t len);
+
+private:
+    rtc::BufferQueue _que;
+    rtc::StreamState _state = rtc::SS_OPEN;
+    uvcore::Udp* _udp;
+    uvcore::IpAddress _remote_addr;
+
+};
 
 class RtcDtls
 {
@@ -15,17 +45,21 @@ public:
         k_failed,
         k_num_values
     };
+    RtcDtls(uvcore::Udp*, const uvcore::IpAddress& remote_addr);
 	static bool is_dtls(const uint8_t* data, size_t len);
     static bool is_dtls_client_hello_packet(const uint8_t* data, size_t len);
 
     void set_remote_dtls(const std::string& alg, const std::string& fp);
-
+    bool setup_dtls();
     void handle_dtls(const uint8_t* data, size_t len);
 
 private:
     std::string _remote_alg;
     std::string _remote_fp;
     DtlsTransportState _state = DtlsTransportState::k_new;
+    std::unique_ptr<rtc::SSLStreamAdapter> _dtls_adapter;
+    DtlsStreamInterface* _downward = nullptr;
+    std::vector<int> _srtp_ciphers;
 };
 
 #endif
